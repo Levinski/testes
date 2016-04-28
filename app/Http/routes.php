a95382e0cfa6317b483bdcb3248cd56a180dda99
Jhonatan Levinski <jhonatanlevinski@hotmail.com>
2016-04-28 10:16:19 -0300

    valores da dependencia alterando automaticamente

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,3 +40,74 @@ Route::get('/1', function () {
 
     echo (new BasicInspetionAndOilChangeAndTireRotation)->getCost();
 });
+
+Route::get('/2', function () {
+
+    interface CarService {
+
+        public function getCost();
+
+        public function getDescription();
+    }
+
+    class BasicInspetion implements CarService {
+
+        public function getCost()
+        {
+            return 25;
+        }
+
+        public function getDescription()
+        {
+            return 'Basic inspection';
+        }
+    }
+
+    class OilChange implements CarService {
+
+        protected $carService;
+
+        function __construct(CarService $carService)
+        {
+            $this->carService = $carService;
+        }
+
+        public function getCost()
+        {
+            return $this->carService->getCost() + 20;
+        }
+
+        public function getDescription()
+        {
+            return $this->carService->getDescription() . ', and oil change';
+        }
+    }
+
+    class TireRotation implements CarService {
+
+        protected $carService;
+
+        function __construct(CarService $carService)
+        {
+            $this->carService = $carService;
+        }
+
+        public function getCost()
+        {
+            return $this->carService->getCost() + 30;
+        }
+
+        public function getDescription()
+        {
+            return $this->carService->getDescription() . ', and tire rotation';
+        }
+    }
+
+    $carService = (new TireRotation(new OilChange(new BasicInspetion)));
+
+    echo $carService->getDescription() . ': $' .  $carService->getCost();
+
+    // Nesse caso nao precisamos alterar manualmente o valor os valores,
+    // se altera em um, todas as dependencias vao trabalhar com esse novo valor,
+    // isso é oque um decorator deve fazer
+});
