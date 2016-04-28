377a51139b6000ec42c379cc31ab1018285c7ab4
Jhonatan Levinski <jhonatanlevinski@hotmail.com>
2016-04-28 16:48:15 -0300

    pattern

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,77 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/1', function () {
+
+    abstract class HomeChecker {
+
+        protected $successor;
+
+        abstract public function check(HomeStatus $home);
+
+        public function setSuccessor(HomeChecker $successor) {
+            $this->successor = $successor;
+        }
+
+        public function next(HomeStatus $home)
+        {
+            if ($this->successor) {
+                $this->successor->check($home);
+            }
+        }
+    }
+
+    class Locks extends HomeChecker {
+
+        public function check(HomeStatus $home)
+        {
+            if (! $home->locked) {
+                throw new Exception('The doors are open');
+            }
+
+            $this->next($home);
+        }
+    }
+
+    class Lights extends HomeChecker {
+
+        public function check(HomeStatus $home)
+        {
+            if (! $home->lightsOff) {
+                throw new Exception('The lights are on');
+            }
+
+            $this->next($home);
+        }
+    }
+
+    class Alarms extends HomeChecker {
+
+        public function check(HomeStatus $home)
+        {
+            if (! $home->alarmOn) {
+                throw new Exception('The alarm is off');
+            }
+
+            $this->next($home);
+        }
+    }
+
+    class HomeStatus {
+        public $alarmOn = true;
+        public $lightsOff = true;
+        public $locked = false;
+    }
+
+    $lights = new Lights;
+    $locks = new Locks;
+    $alarms = new Alarms;
+
+    $lights->setSuccessor($locks);
+    $locks->setSuccessor($alarms);
+
+    $lights->check(new HomeStatus);
+
+    echo 'no problem';
+});
