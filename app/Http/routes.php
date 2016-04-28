57baa7fd8bf465043d0910c70ac811bc65fce877
Jhonatan Levinski <jhonatanlevinski@hotmail.com>
2016-04-28 13:56:01 -0300

    no-pattern

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,76 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// No-pattern
+Route::get('/1', function () {
+
+    abstract class VeganSub {
+
+        public function make()
+        {
+            return $this
+                ->layBread()
+                ->addVegetables()
+                ->addSoja();
+        }
+
+        public function layBread()
+        {
+            var_dump('laying down the bread');
+
+            return $this;
+        }
+
+        public function addVegetables()
+        {
+            var_dump('add some vegetables');
+
+            return $this;
+        }
+
+        public function addSoja()
+        {
+            var_dump('add some soja');
+
+            return $this;
+        }
+    }
+
+    class CheeseSub {
+
+        public function make()
+        {
+            return $this
+                ->layBread()
+                ->addCheese()
+                ->addSoja();
+        }
+
+        public function layBread()
+        {
+            var_dump('laying down the bread');
+
+            return $this;
+        }
+
+        public function addCheese()
+        {
+            var_dump('add some cheese');
+
+            return $this;
+        }
+
+        public function addSoja()
+        {
+            var_dump('add some soja');
+
+            return $this;
+        }
+    }
+
+
+    (new CheeseSub)->make();
+    echo '<br>';
+    (new VeganSub)->make();
+});
